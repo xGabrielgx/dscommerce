8709d0632a698cca27c8785b2d04851c1435460b
Gabriel Xavier <gabrielgx928@gmail.com>
2025-05-28 11:55:08 -0300

    Get user logged

---
+++ src/main/java/com/projetoestudos/dscommerce/controllers/UserController.java
@@ -0,0 +1,32 @@
+package com.projetoestudos.dscommerce.controllers;
+
+import com.projetoestudos.dscommerce.dto.ProductDTO;
+import com.projetoestudos.dscommerce.dto.UserDTO;
+import com.projetoestudos.dscommerce.services.ProductService;
+import com.projetoestudos.dscommerce.services.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
+    @GetMapping(value = "/me")
+    public ResponseEntity<UserDTO> getMe() {
+        UserDTO dto = service.getMe();
+        return ResponseEntity.ok(dto);
+    }
+
+}
